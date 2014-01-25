f39404edda93922d0a0690e8ec44d35195755e32
Noah Lemen <noah.lemen@gmail.com>
2014-01-25 12:12:06 -0500

    player scripts

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform tofollow;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(tofollow.position.x, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void FixedUpdate(){
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		Debug.Log("hit platform");
+		transform.rotation = c.transform.rotation;
+		//rigidbody2D.AddForce(transform.right * speed);
+		rigidbody2D.velocity = transform.right * speed;
+	}
+
+	void OnCollisionStay2D(Collision2D c){
+		//rigidbody2D.AddForce(transform.right * speed);
+	}
+
+}
