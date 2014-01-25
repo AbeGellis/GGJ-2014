7726128729e4278454aab4cc1ea06a32c118ff85
Noah Lemen <noah.lemen@gmail.com>
2014-01-25 13:19:25 -0500

    screenshake, camera follow, movement fixes

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,6 +7,22 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(tofollow.position.x, transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp(transform.position, new Vector3(tofollow.transform.position.x, transform.position.y, transform.position.z), 1f);
 	}
+
+	public void Shake(float x, float y, float t){
+		StartCoroutine(ScreenShake(x, y, t));
+	}
+
+	IEnumerator ScreenShake (float x, float y, float t){
+
+		while(t > 0f){
+			t -= Time.deltaTime;
+			Camera.main.transform.position += t *
+				new Vector3(Mathf.Sin (Time.time*100f) * x, 
+				            Mathf.Sin (Time.time*100f) * y, 0f); 
+			yield return 0;
+		}
+	}
+
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,14 +9,14 @@ public class PlayerMove : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D c){
+		Camera.main.GetComponent<CameraFollow>().Shake(.1f,.1f,.2f);
 		Debug.Log("hit platform");
 		transform.rotation = c.transform.rotation;
-		//rigidbody2D.AddForce(transform.right * speed);
 		rigidbody2D.velocity = transform.right * speed;
 	}
 
 	void OnCollisionStay2D(Collision2D c){
-		//rigidbody2D.AddForce(transform.right * speed);
+		rigidbody2D.velocity = transform.right * speed;
 	}
 
 }
