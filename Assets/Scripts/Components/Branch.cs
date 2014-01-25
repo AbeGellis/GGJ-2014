26c3ad3ecb7eb0a6ed37f6746aa3919ee87d1f4d
Abe Gellis <aagellis@gmail.com>
2014-01-25 15:03:42 -0500

    Functioning path generation, including transitioning colors.

---
+++ Assets/Scripts/Components/Branch.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Branch : Transition {
+
+	public GameObject root;
+	public GameObject path1;
+	public GameObject path2;
+
+	public GameObject end1;
+	public GameObject end2;
+
+	override public void SetStartColor(Platform.PlatColor c) {
+		base.SetStartColor (c);
+		startColor = c;
+		
+		Platform.PlatColor p, n;
+		bool choice = (Random.Range(0, 2) == 1);
+		switch (startColor) {
+		case Platform.PlatColor.Blue:
+			p = choice ? Platform.PlatColor.Violet : Platform.PlatColor.Green;
+			n = choice ? Platform.PlatColor.Red : Platform.PlatColor.Yellow;
+			break;
+		case Platform.PlatColor.Red:
+			p = choice ? Platform.PlatColor.Violet : Platform.PlatColor.Orange;
+			n = choice ? Platform.PlatColor.Blue : Platform.PlatColor.Yellow;
+			break;
+		case Platform.PlatColor.Yellow:
+			p = choice ? Platform.PlatColor.Orange : Platform.PlatColor.Green;
+			n = choice ? Platform.PlatColor.Red : Platform.PlatColor.Blue;
+			break;
+		default:
+			p = c;	//This should not happen and I don't care what happens if it does
+			n = c;
+			break;
+		}
+
+		choice = Random.Range (0, 2) == 1;
+		root.GetComponent<Platform>().color = p;
+		path1.GetComponent<Platform>().color = choice ? n : c;
+		path2.GetComponent<Platform>().color = choice ? c : n;
+		end1.GetComponent<EndNode>().nextColor = choice ? n : c;
+		end2.GetComponent<EndNode>().nextColor = choice ? c : n;
+	}
+}
--- Assets/Scripts/EndNode.cs
+++ Assets/Scripts/EndNode.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class EndNode : MonoBehaviour {
 
 
-	bool grown = false;
+	public bool grown = false;
+	public Platform.PlatColor nextColor;
+	public bool transitionCanFollow = true;
 
 	void Start () {
 	}
@@ -19,13 +21,28 @@ public class EndNode : MonoBehaviour {
 			}
 		} else {	//Destroy platform if past view
 			float camTrailingEdge = Camera.main.ViewportToWorldPoint (Vector3.zero).x;
-			if (transform.position.x < camTrailingEdge)
+			if (transform.position.x < (camTrailingEdge - GrowControl.KILLTHRESHOLD))
 				Destroy(transform.parent.gameObject);
 		}
 	}
 
 	void Grow() {
-		GameObject ext = Instantiate(GrowControl.getExtension ()) as GameObject;
-		ext.transform.position = transform.position;
+		if (!Physics2D.Raycast(transform.position + Vector3.right/5f, Vector2.zero)) {
+			if (transform.position.y > GrowControl.BOTTOM) {
+				if (transform.position.y < GrowControl.TOP) {
+					GameObject ext = Instantiate(GrowControl.getExtension(transitionCanFollow)) as GameObject;
+					ext.transform.position = transform.position;
+					ext.GetComponent<PlatComponent>().SetStartColor(nextColor);
+				} else {
+					GameObject ext = Instantiate(GrowControl.getExtDown()) as GameObject;
+					ext.transform.position = transform.position;
+					ext.GetComponent<PlatComponent>().SetStartColor(nextColor);
+				}
+			} else {
+				GameObject ext = Instantiate(GrowControl.getExtUp()) as GameObject;
+				ext.transform.position = transform.position;
+				ext.GetComponent<PlatComponent>().SetStartColor(nextColor);
+			}
+		}
 	}
 }
