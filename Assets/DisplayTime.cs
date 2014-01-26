230a5af58d4a9fcb36d5d74eecc530ca9d52cdd4
Noah Lemen <noah.lemen@gmail.com>
2014-01-26 17:13:07 -0500

    seconds

--- Assets/DisplayTime.cs
+++ Assets/DisplayTime.cs
@@ -6,7 +6,7 @@ public class DisplayTime : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		float score = Timer.timer;
-		GetComponent<TextMesh>().text = score.ToString();
+		GetComponent<TextMesh>().text = score.ToString() + "\nseconds";
 	}
 	
 
---
+++ Assets/offon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class offon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<BoxCollider2D>().enabled = false;
+		GetComponent<BoxCollider2D>().enabled = true;
+		GetComponent<CircleCollider2D>().enabled = false;
+		GetComponent<CircleCollider2D>().enabled = true;
+
+	}
+}
