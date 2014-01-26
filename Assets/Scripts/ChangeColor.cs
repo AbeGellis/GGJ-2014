da225e8a2e18be26fe38c1ba83d2946d2d7631b5
Noah Lemen <noah.lemen@gmail.com>
2014-01-25 21:35:26 -0500

    sounds

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class ChangeColor : MonoBehaviour {
 
 	public KeyCode red, yellow, blue;
+	AudioSource a;
 
 	void Start () {
 		renderer.material.color = Color.red;
 		gameObject.layer = LayerMask.NameToLayer("Red");
+		a = GetComponent<AudioSource>();
 	}
 
 	void Update () {
@@ -19,13 +21,19 @@ public class ChangeColor : MonoBehaviour {
 	public Color getPlayerColor(){
 		if(Input.GetKeyDown(red)){
 			gameObject.layer = LayerMask.NameToLayer("Red");
+			a.Play();
 			return Color.red;
+
 		}else if(Input.GetKeyDown(yellow)){
 			gameObject.layer = LayerMask.NameToLayer("Yellow");
+			a.Play();
 			return Color.yellow;
+
 		}else if(Input.GetKeyDown(blue)){
 			gameObject.layer = LayerMask.NameToLayer("Blue");
+			a.Play();
 			return Color.blue;
+
 		}else return renderer.material.color;
 	}
 
